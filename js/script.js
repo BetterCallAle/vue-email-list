1ca52a30869e67a49b7bdf482723ff58607a2f67
BetterCallAle <dap22091998@gmail.com>
2022-10-28 16:17:19 +0200

    5 emails are now visible on page

--- js/script.js
+++ js/script.js
@@ -3,7 +3,25 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
+            randomEmail: [],
+            thisEmail: ""
+        }
+    },
+
+    created(){ 
             
+        this.getRndEmail()
+    },
+
+    methods:{
+        getRndEmail(){
+           for (let i = 0; i < 5; i++) {
+                axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((resp)=>{
+                        this.randomEmail.push(resp.data.response)
+                })
+           }
         }
     }
 }).mount("#app")
